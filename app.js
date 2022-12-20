baadcca4530b5a724cf4e183a111d137f5098662
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-12-20 14:58:16 -0300

    Adicionando as informações do mailtrap na porta send-email

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 import express from "express";
+import nodemailer from "nodemailer";
 
 const app = express();
 const port = 3000;
 
 app.get("/send-email", async (req, res) => {
+  var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "218cedba8e8a68",
+      pass: "27ca1a74ad3b17",
+    },
+  });
+
   try {
     return res.status(200).json({ message: "E-mail enviado com sucesso!!!" });
   } catch (erro) {
