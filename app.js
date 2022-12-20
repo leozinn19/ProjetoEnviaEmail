be974512d0f5a89bb8d1921ba2e019a838e474f3
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-12-20 14:33:17 -0300

    Primeira porta do servidor: '/'

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Qualé fi");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
