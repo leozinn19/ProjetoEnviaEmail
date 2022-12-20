6481cb1338e3392207f6c11eddbc961b6c78cd7d
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2022-12-20 14:52:39 -0300

    Adicionando o dev nodemon e a porta send-email

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/send-email", async (req, res) => {
+  try {
+    return res.status(200).json({ message: "E-mail enviado com sucesso!!!" });
+  } catch (erro) {
+    return res.status(500);
+  }
+});
+
 app.get("/", (req, res) => {
   res.send("Qualé fi");
 });
